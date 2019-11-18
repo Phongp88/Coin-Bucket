0c88658c0022c6b969a31cbd2cd771af28f30d02
Connor Anderson <connorbanderson@gmail.com>
2019-11-19 01:04:30 +0700

    Index - Redux Provider, Theme Provider and Router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import configureStore from "./Redux/store";
+import * as serviceWorker from "./serviceWorker";
+import App from "./App";
+import "./Styles/global.scss";
+import {
+  COIN_BUCKET_PRIMARY_COLOR,
+  COIN_BUCKET_SECONDARY_COLOR
+} from "./constants";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = configureStore();
+
+const history = createBrowserHistory();
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: COIN_BUCKET_PRIMARY_COLOR },
+    secondary: { main: COIN_BUCKET_SECONDARY_COLOR }
+  },
+  typography: {
+    fontFamily: ["Poppins"].join(",")
+  }
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <Router history={history}>
+        <Route path="/" component={App} />
+      </Router>
+    </ThemeProvider>
+  </Provider>,
+  document.getElementById("root")
+);
+
+serviceWorker.register();
