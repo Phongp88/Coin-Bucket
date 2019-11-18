6ec60e7051a7a13c88512fe757d1031c8add9075
Connor Anderson <connorbanderson@gmail.com>
2019-11-19 01:04:49 +0700

    Dashboard Page

---
+++ src/Pages/Auth/Dashboard/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Dashboard.scss";
+
+const Dashboard = () => {
+  return (
+    <section className="dashboard">
+      <span> Dashboard </span>
+    </section>
+  );
+};
+
+export default Dashboard;
