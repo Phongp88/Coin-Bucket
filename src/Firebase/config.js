2a36b234d5eec13685118b25284032e8465b6537
phong <phong_pham88@hotmail.com>
2019-11-19 23:50:46 -0700

    hooked up firebase config

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -1,13 +1,14 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDI0YIl0T3beSZ9esIlRL6uMn4XwrfHK6o",
-  authDomain: "servo-53090.firebaseapp.com",
-  databaseURL: "https://servo-53090.firebaseio.com",
-  projectId: "servo-53090",
-  storageBucket: "",
-  messagingSenderId: "281208039125",
-  appId: "1:281208039125:web:971d60793277db45"
+var firebaseConfig = {
+  apiKey: "AIzaSyD1xCz6_j4Rw2ztNNvLK7t-4JBMVqW0cXs",
+  authDomain: "coin-bucket-81d62.firebaseapp.com",
+  databaseURL: "https://coin-bucket-81d62.firebaseio.com",
+  projectId: "coin-bucket-81d62",
+  storageBucket: "coin-bucket-81d62.appspot.com",
+  messagingSenderId: "529148600662",
+  appId: "1:529148600662:web:60caef23264ca4b35436f7",
+  measurementId: "G-ZLMBH909MT"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
