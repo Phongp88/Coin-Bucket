bd1d3ea606e8061fae50d6699db44e9fca1cef12
phong <phong_pham88@hotmail.com>
2019-11-19 23:52:37 -0700

    added redux tools

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,7 +1,13 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./Reducers/index";
 
-const configureStore = () => createStore(rootReducer, applyMiddleware(thunk));
+
+const middleware = [thunk];
+
+const configureStore = () => createStore(rootReducer, compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  ));
 
 export default configureStore;
