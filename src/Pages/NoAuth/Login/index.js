15263c311565fc9408ba986e57a7e605b55873bb
phong <phong_pham88@hotmail.com>
2019-11-19 23:52:51 -0700

    User can login and be redirected

--- src/Pages/NoAuth/Login/index.js
+++ src/Pages/NoAuth/Login/index.js
@@ -2,11 +2,20 @@ import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 
+
+// Redux 
+import { connect } from "react-redux";
+import { login } from "../../../Redux/Actions/auth";
+// CSS
 import "./Login.scss";
 
-const Login = () => {
-  const [email, setEmail] = useState(undefined);
-  const [password, setPassword] = useState(undefined);
+const Login = ({login}) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleLogin () {
+    login(email, password);
+  }
   return (
     <section className="login">
       <div className="login__formWrapper">
@@ -14,16 +23,16 @@ const Login = () => {
           autoFocus
           label="Email"
           type="email"
-          onChange={email => setEmail(email)}
+          onChange={(e) => setEmail(e.target.value)}
           //handleSubmit={e => [e.preventDefault(), this.handleLoginRequest()]}
         />
         <TextField
           label="Password"
           type="password"
-          onChange={password => setPassword(password)}
+          onChange={(e) => setPassword(e.target.value)}
           //handleSubmit={e => [e.preventDefault(), this.handleLoginRequest()]}
         />
-        <Button variant="contained" color="primary">
+        <Button variant="contained" color="primary" onClick={()=>handleLogin()}>
           Log In
         </Button>
       </div>
@@ -31,4 +40,12 @@ const Login = () => {
   );
 };
 
-export default Login;
+const mapStateToProps = state => ({
+  user: state.user
+})
+
+const mapDispatchToProps = dispatch => ({
+  login: (email, password) => dispatch(login(email, password))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
